12a047af2b68c15d66f90587e1dca41a62279339
omershohat <iomershohat@gmail.com>
2025-05-22 18:53:49 +0300

    fixing branches

--- src/Omer_Ran/Professor.java
+++ src/Omer_Ran/Professor.java
@@ -1,7 +1,7 @@
 package Omer_Ran;
 
 public class Professor extends Doctor {
-    private String certifyingInstitution;
+    private String certifyingInst;
     public Professor(String name, String id, DegreeLevel degreeLevel, String major, float salary, Department department, String certifyingInstitution) {
         super(name, id, degreeLevel, major, salary, department);
     }
